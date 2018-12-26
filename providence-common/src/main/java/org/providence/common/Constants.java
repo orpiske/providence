44332a3b9024c5e93adbeb7a89b57d2284d05216
Otavio R. Piske <angusyoung@gmail.com>
2018-12-26 23:30:18 +0100

    UPD: more WIP code

---
+++ providence-common/src/main/java/org/providence/common/Constants.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2018 Otavio R. Piske <angusyoung@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.providence.common;
+
+import java.io.File;
+
+/**
+ * Application constants
+ */
+public final class Constants {
+
+    public static final String VERSION = "0.0.1-SNAPSHOT";
+
+    public static final int VERSION_NUMERIC;
+
+    public static final String BIN_NAME = "providence";
+
+    public static final String HOME_PROPERTY = "org.providence.home";
+
+    public static final String HOME_DIR;
+
+    public static final String PROVIDENCE_CONFIG_DIR;
+
+    public static final String PROVIDENCE_LOG_DIR;
+
+    static {
+        HOME_DIR = System.getProperty(HOME_PROPERTY);
+
+        PROVIDENCE_CONFIG_DIR = System.getProperty(HOME_PROPERTY) + File.separator + "config";
+
+        PROVIDENCE_LOG_DIR = System.getProperty(HOME_PROPERTY) + File.separator + "logs";
+
+        VERSION_NUMERIC = Integer.parseInt(VERSION.replace(".", "").replaceAll("[a-zA-Z-]",""));
+    }
+
+
+    /**
+     * Restricted constructor
+     */
+    private Constants() {
+    }
+
+
+}
---
+++ providence-common/src/main/java/org/providence/common/LogConfigurator.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright 2018 Otavio R. Piske <angusyoung@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.providence.common;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * Utility class to configure the logger
+ */
+public class LogConfigurator {
+
+    /**
+     * Restricted constructor
+     */
+    private LogConfigurator() {}
+
+
+    private static void configureCommon(Properties properties) {
+        properties.setProperty("log4j.appender.stdout",
+                "org.apache.log4j.ConsoleAppender");
+
+        properties.setProperty("log4j.appender.stdout.Target",
+                "System.out");
+        properties.setProperty("log4j.appender.stdout.layout",
+                "org.apache.log4j.PatternLayout");
+        properties.setProperty(
+                "log4j.appender.stdout.layout.ConversionPattern",
+                "%C.%M:%L [%p] %m%n");
+        properties.setProperty("log4j.additivity.org.providence", "false");
+    }
+
+    private static void configureTrace(Properties properties) {
+        properties.setProperty("log4j.rootLogger", "DEBUG, stdout");
+        properties.setProperty("log4j.logger.org.providence", "TRACE, stdout");
+
+        properties.setProperty(
+                "log4j.appender.stdout.layout.ConversionPattern",
+                "%d{HH:mm:ss,SSS} %C.%M:%L [%p] %m%n");
+    }
+
+    private static void configureDebug(Properties properties) {
+        properties.setProperty("log4j.rootLogger", "INFO, stdout");
+        properties.setProperty("log4j.logger.org.providence", "DEBUG, stdout");
+        properties.setProperty(
+                "log4j.appender.stdout.layout.ConversionPattern",
+                "%d{HH:mm:ss,SSS} %C.%M:%L [%p] %m%n");
+    }
+
+    private static void configureVerbose(Properties properties) {
+        properties.setProperty("log4j.rootLogger", "WARN, stdout");
+        properties.setProperty("log4j.logger.org.providence", "INFO, stdout");
+
+        properties.setProperty(
+                "log4j.appender.stdout.layout.ConversionPattern",
+                "[%p] %m%n");
+    }
+
+    private static void configureSilent(Properties properties) {
+        properties.setProperty("log4j.rootLogger", "WARN, stdout");
+        properties.setProperty("log4j.logger.org.providence", "WARN, stdout");
+    }
+
+    /**
+     * Default log configuration for the daemon
+     */
+    public static void defaultForDaemons() {
+        PropertyConfigurator.configure(Constants.PROVIDENCE_CONFIG_DIR
+                + File.separator + "log4j.properties");
+    }
+
+    /**
+     * Configure the output to be at trace level
+     */
+    public static void trace() {
+        Properties properties = new Properties();
+
+        configureCommon(properties);
+        configureTrace(properties);
+
+        PropertyConfigurator.configure(properties);
+    }
+
+
+    /**
+     * Configure the output to be at debug level
+     */
+    public static void debug() {
+        Properties properties = new Properties();
+
+        configureCommon(properties);
+        configureDebug(properties);
+
+        PropertyConfigurator.configure(properties);
+    }
+
+
+    /**
+     * Configure the output to be at verbose (info) level
+     */
+    public static void verbose() {
+        Properties properties = new Properties();
+
+        configureCommon(properties);
+        configureVerbose(properties);
+
+        PropertyConfigurator.configure(properties);
+    }
+
+
+    /**
+     * Configure the output to be as silent as possible
+     */
+    public static void silent() {
+        Properties properties = new Properties();
+
+        configureCommon(properties);
+        configureSilent(properties);
+
+        PropertyConfigurator.configure(properties);
+    }
+
+
+    /**
+     * Configures the log level based on the input string
+     * @param logLevel The log level  as a string (one of trace, debug, warn, info).
+     */
+    public static void configureLogLevel(final String logLevel) {
+        if (logLevel == null) {
+            LogConfigurator.verbose();
+            return;
+        }
+
+        switch (logLevel) {
+            case "trace": {
+                LogConfigurator.trace();
+                break;
+            }
+            case "debug": {
+                LogConfigurator.debug();
+                break;
+            }
+            case "warn": {
+                LogConfigurator.silent();
+                break;
+            }
+            case "custom": {
+                LogConfigurator.defaultForDaemons();
+                break;
+            }
+            case "info":
+            default: {
+                LogConfigurator.verbose();
+                break;
+            }
+        }
+    }
+}
