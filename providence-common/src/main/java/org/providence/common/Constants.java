99fe820ff364b762c470ca078d138047d1ddf5c9
Otavio R. Piske <angusyoung@gmail.com>
2021-08-01 10:47:30 +0200

    Version bump to 0.0.6-SNAPSHOT for development

--- providence-common/src/main/java/org/providence/common/Constants.java
+++ providence-common/src/main/java/org/providence/common/Constants.java
@@ -22,7 +22,7 @@ import java.io.File;
  */
 public final class Constants {
 
-    public static final String VERSION = "0.0.1-SNAPSHOT";
+    public static final String VERSION = "0.0.6-SNAPSHOT";
 
     public static final int VERSION_NUMERIC;
 
