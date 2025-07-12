e52d51cdfa623bf615655df6b4707e32ff668f74
Otavio R. Piske <angusyoung@gmail.com>
2025-07-12 07:11:43 +0200

    Do create the DB on an entirely new system

---
+++ core/providence-common/src/main/resources/org/providence/common/dao/builder/create-db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `shared` (
+                                        `shared_id` INT NOT NULL AUTO_INCREMENT,
+                                        `shared_source` VARCHAR(128) NOT NULL,
+    `shared_format` VARCHAR(128) NOT NULL,
+    `shared_date` TIMESTAMP NOT NULL DEFAULT NOW(),
+    `shared_text` VARCHAR(4096) NULL,
+    PRIMARY KEY (`shared_id`));
+
+ALTER TABLE `shared` ADD COLUMN IF NOT EXISTS `shared_hash` LONG DEFAULT 0 AFTER `shared_id`;
